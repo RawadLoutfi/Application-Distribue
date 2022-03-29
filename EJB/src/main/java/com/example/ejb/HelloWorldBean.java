6facbb8b8d19505862c24b2190768d2a4725be78
Rawad Loutfi <rawad.loutfi1@gmail.com>
2022-03-29 19:44:27 +0300

    devoir EJB

--- EJB/src/main/java/com/example/ejb/HelloWorldBean.java
+++ EJB/src/main/java/com/example/ejb/HelloWorldBean.java
@@ -2,7 +2,7 @@ package com.example.ejb;
 import javax.ejb.*;
 
 @Stateless
-public class HelloWorldBean implements HelloWorld{
+public class HelloWorldBean implements DevoirEJB{
     @Override
     public String sayHello() {
         return "Mme Rima !!!";
