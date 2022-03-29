832ce1fd15f699a0251e625763e14b0101a68f43
Rawad Loutfi <rawad.loutfi1@gmail.com>
2022-03-29 19:43:24 +0300

    devoir EJB

--- EJB/src/main/java/com/example/ejb/HelloServlet.java
+++ EJB/src/main/java/com/example/ejb/HelloServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private HelloWorld msg;
+    private DevoirEJB msg;
 
     public void init() {
         msg = new HelloWorldBean();
